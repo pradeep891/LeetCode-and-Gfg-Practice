7d00629d9976a12128568d2a4065265503226a02
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-09-10 17:24:30 +0530

    Time: 97 ms (11.25%), Space: 21.3 MB (6.51%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
-    
-    int ar[1001][101][2] = {-1};
-    int solve(vector<int>&prices , int k ,int id , int bs){
-        
-        if(id == prices.size() || k == 0)
-            return 0;
-        // bs == 0 // buy 
-        
-        if(ar[id][k][bs] != -1)
-            return ar[id][k][bs];
-        
-        int ans = 0;
-        if(bs == 0){
-            int a = -prices[id] + solve(prices , k , id+1 , 1);
-            int b = solve(prices , k , id+1 , bs);
-            ans = max(a , b);
-        }
-        else{            
-            int a = prices[id] + solve(prices , k-1 , id+1 , 0);
-            int b = solve(prices , k , id+1 , 1);
-            ans = max(a , b);
-        }
-        // return ans;
-        return ar[id][k][bs] = ans;
-    }
     int maxProfit(int k, vector<int>& prices) {
         
-        for(int i=0; i<1001 ; i++){
-        // int i = 0;
-            for(int k=0; k<101; k++){
-                for(int b = 0; b<2; b++){
-                    ar[i][k][b] = -1;
-                }
-                // cout << endl;
+        vector<vector<vector<int>>>dp(prices.size()+1 , vector<vector<int>>(k+1 , vector<int>(2 , 0)));
+        
+        for(int i=prices.size() - 1 ; i>=0; i--){
+            for(int j =1; j<=k; j++){
+                dp[i][j][0] = max( -prices[i] + dp[i+1][j][1] , dp[i+1][j][0]);
+                dp[i][j][1] = max( prices[i] + dp[i+1][j-1][0] , dp[i+1][j][1]);
             }
         }
-        return solve(prices , k , 0 , 0);
+        return dp[0][k][0];
     }
 };
