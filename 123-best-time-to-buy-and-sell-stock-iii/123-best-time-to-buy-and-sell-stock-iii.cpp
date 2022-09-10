38a7be02c6b22d53c47cc29557ca5e89b11f0afa
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-09-10 16:22:10 +0530

    Time: 1857 ms (5.03%), Space: 272.9 MB (5.03%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -5,12 +5,10 @@ public:
         vector< vector< vector<int> > > dp(prices.size() + 1 , vector<vector<int>>(2+1 , vector<int>(2 , 0)));
         for(int i=prices.size() - 1 ; i>=0; i--){
             for(int j=1; j<=2; j++){
-                for(int k = 0 ; k<2; k++){
-                    if(k == 0)
-                        dp[i][j][k] = max( -prices[i] + dp[i+1][j][1] , dp[i+1][j][0] );
-                    else
-                        dp[i][j][k] = max( prices[i] + dp[i+1][j-1][0] , dp[i+1][j][1] );  
-                }
+                
+                        dp[i][j][0] = max( -prices[i] + dp[i+1][j][1] , dp[i+1][j][0] );
+                        dp[i][j][1] = max( prices[i] + dp[i+1][j-1][0] , dp[i+1][j][1] );  
+                
             }
         }
         return dp[0][2][0]; //solve(prices, 0 , 2 , 0 , dp);
