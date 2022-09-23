f0cdf0fb0178411879751f5348fc2a384fa16545
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-09-23 11:04:29 +0530

    Time: 368 ms (32.61%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
     
-    int noOfBit(int n){
-        int ct = 0;
-        while(n){
-            ct++;
-            n /= 2;
-        }
-        return ct ;
-    }
-    
-    int power(long long a , int n , long long mod){
-        long long res = 1;
-        while(n){
-            if(n % 2 == 0){
-                a = (a * a) % mod;
-                n /= 2;
-            }
-            else{
-                res = (res * a) % mod;
-                n--;
-            }
-        }
-        return res;
-    }
     int concatenatedBinary(int n) {
         long long ans = 1;
         long long mod = 1e9+7;
         for(int i=2; i<=n ;i++){
-            int bit = noOfBit(i);
-            // cout << bit << ", ";
-            ans = ans * power(2 , bit , mod);
-            ans = (ans + i) % mod;
-            // cout << ans << " | ";
+            int j = i;
+            while(j){
+                ans = (ans << 1) % mod;
+                j /= 2;
+            }
+            ans = (ans + i ) % mod;
         }
-        // cout << endl;
         return ans % mod;
     }
 };
