f5a02bc7b8c953249f6df5b013b8a8ca48c774b2
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-10-06 14:38:42 +0530

    Time: 778 ms (17.47%), Space: 128.7 MB (47.07%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -0,0 +1,51 @@
+class TimeMap {
+public:
+    
+    
+    map<string , vector< pair<string , int>> >mp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {        
+        mp[key].push_back({value , timestamp});
+    }
+    
+    string binarysearch(vector< pair<string , int> >&v , int len , int time){
+        int i=0 , j=len-1;
+        string ans = "";
+        while(i<=j){
+            int mid = i + (j - i) / 2;
+            if(v[mid].second <= time)
+            {
+                ans = v[mid].first;
+                i = mid+1;
+            }
+            else{
+                j = mid-1;
+            }
+        }
+        return ans;
+    }
+    string get(string key, int timestamp) {
+        if(mp.find(key) == mp.end())
+            return "";
+        
+        // string s = "";
+        int len = mp[key].size();
+        
+        return binarysearch(mp[key] , len , timestamp);
+        // for(auto i : mp[key]){
+        //     if(i.second <= timestamp)
+        //         s = i.first;
+        // }
+        // return s;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
