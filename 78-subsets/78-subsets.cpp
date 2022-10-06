1c391209474ebec36a576d48de5f89fa664b1dd1
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-10-06 23:50:06 +0530

    Time: 7 ms (37.39%), Space: 7.2 MB (70.52%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     
-    void solve(vector<int>&nums , int id , vector<vector<int>>&res , vector<int>&cur){
-        int n = nums.size();
-        if(id == n) {
-            res.push_back(cur);
-            return;
-        }
+//     void solve(vector<int>&nums , int id , vector<vector<int>>&res , vector<int>&cur){
+//         int n = nums.size();
+//         if(id == n) {
+//             res.push_back(cur);
+//             return;
+//         }
         
-        solve(nums , id+1 , res , cur);        
-        cur.push_back(nums[id]);
-        solve(nums , id+1 , res , cur);
-        cur.pop_back();
-    }
+//         solve(nums , id+1 , res , cur);        
+//         cur.push_back(nums[id]);
+//         solve(nums , id+1 , res , cur);
+//         cur.pop_back();
+//     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>cur;
+        int len = nums.size();
+        int n = pow(2, len);
+        
         vector< vector<int>>res;
-        // vector<int>vis(nums.size() , 0);
-        solve(nums , 0 , res , cur);
+        // cout << "n = " << n << endl;
+        for(int i=0; i<n ;i++){
+            // cout << i <<  " ";
+            int k = i;
+            vector<int>cur;
+            for(int j=0; j<len; j++){
+                
+                int p = 1 << (len-j-1);
+                int r = k & p;
+                // cout << p << " " <<  r << " | ";
+                if(r == p)
+                    cur.push_back(nums[j]);
+            }
+            // cout << endl;
+            res.push_back(cur);
+        }
+        // vector<int>cur;
+        // vector< vector<int>>res;
+        // // vector<int>vis(nums.size() , 0);
+        // solve(nums , 0 , res , cur);
         return res;
     }
 };
