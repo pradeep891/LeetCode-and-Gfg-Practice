234a40c3c59c2dd8b069f147d2291dd143884bc4
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-10-20 01:34:11 +0530

    Time: 47 ms (6.01%), Space: 12.6 MB (77.02%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,42 +1,28 @@
-class Solution {
-public:
-    bool static cmp(pair<int , string> &a , pair<int,  string>&b){
+struct cmp{
+    bool operator() (pair<int , string> &a , pair<int,  string>&b)  {        
         if(a.first == b.first) return a.second < b.second;
         return a.first > b.first;
     }
+};
+
+class Solution {
+public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string , int>fre;
         for(auto str : words){
             fre[str]++;
         }
-        // priority_queue< pair<int , string> , vector<pair<int,int> > , cmp>pq;
-        
-        vector< pair<int , string>> v;
-        for(auto i : fre){
-            v.push_back({i.second , i.first});
-//             if(pq.size() <= k){
-//                 pq.push({i.second , i.first});
-//             }
-            
-//             else{
-//                 int f = i.first;
-//                 string str = i.second;
-//                 pair<string , int>top = pq.top();
-//                 if(f < top.second)
-//                     continue;
-                
-//                 // pq.pop();
-//                 if(f == top.second){
-                    
-//                 }
-                
-//             }
-        }
-        
-        sort(v.begin()  , v.end() , cmp);
+        priority_queue< pair<int , string> , vector<pair<int,string> > , cmp>pq;        
+        for(auto cur : fre){      
+            pq.push({cur.second , cur.first});
+            if(pq.size() > k) pq.pop();
+        }        
         vector<string>ans;
-        for(int i=0; i<k ;i++)
-            ans.push_back(v[i].second);
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(ans.begin() , ans.end());
         return ans;
     }
         
