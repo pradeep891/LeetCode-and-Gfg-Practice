41111989ccd928053f093cac9e305af0fbff0e38
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-11-09 19:24:32 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,25 +9,45 @@ using namespace std;
 
 class Solution{   
 public:
-    int solve(int id ,vector<int>&arr, int sum , vector< vector<int> >&dp){
+    int solve(vector<int>&arr, int id , int sum , vector< vector<int> >&dp){
         if(sum == 0) return 1;
-        if(sum < 0) return 0;
-        if(id == arr.size()) return 0;
+        
+        if(id == 0) return 0;
         
         if(dp[id][sum] != -1) return dp[id][sum];
         
-        int pick = solve(id+1 , arr , sum - arr[id] , dp);
-        int notpick = solve(id+1 , arr , sum , dp);
-        int sum2 = pick + notpick;
-        if(sum2 >= 1) 
-        sum2 = 1;
-        return dp[id][sum] = sum2;
+        int pick = 0;
+        
+        if(sum - arr[id-1] >= 0)
+        pick = solve(arr , id-1, sum - arr[id-1] , dp);
+        
+        int notpick = solve( arr , id -1  , sum , dp);
+        
+        return dp[id][sum] = pick || notpick;
+        // return 0;
     }
     
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
-        vector< vector<int> >dp(arr.size() , vector<int>(sum+1 , -1));
-        return solve(0 , arr , sum , dp);
+        vector< vector<int> >dp(arr.size()+1 , vector<int>(sum+1 , -1));
+        // return false;
+        return solve(arr , arr.size() , sum , dp);
+        
+        // for(int i=0; i<arr.size(); i++)
+        // dp[i][0] = 1;
+        
+        // for(int i=arr.size()-1 ; i>=0 ; i--){
+        //     for(int j=1; j<=sum; j++){
+        //         int temp = 0;
+        //         if(j - arr[i] >=0)
+        //         temp = dp[i+1][j - arr[i]];
+        //         temp += dp[i+1][j];
+                
+        //         if(temp >= 1) j = 1;
+        //         dp[i][j] = temp;
+        //     }
+        // }
+        // return dp[0][sum];// solve(0 , arr , sum , dp);
         
     }
 };
