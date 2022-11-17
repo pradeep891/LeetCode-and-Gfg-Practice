ba57249172d58d84c43decad4fe0c655544c03fb
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-11-17 18:04:56 +0530

    Time: 2 ms (95.66%), Space: 8.7 MB (91.39%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
+    
     int findPeakElement(vector<int>& nums) {
-        // int ct = 0;
-        for(int i=1; i<nums.size()-1; i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
+        int i=0 , j = nums.size() - 1;
+        while( i <= j){
+            int m = i + (j - i) / 2;
+            // cout << i << " " << m << " " << j << endl;
+            // if(m+1 > j || m-1 < i || (nums[m] > nums[m+1] && nums[m] > nums[m-1]))
+            //     return m;
+            
+            if(m+1 <= j && m-1 >= i && nums[m] > nums[m+1] && nums[m] > nums[m-1]){
+                return m ;
+                cout << "p ";              
+            }
+            else if(m+1 <= j && nums[m] < nums[m+1]){
+                i = m + 1;
+            }
+            else if(m-1 >= i && nums[m] < nums[m-1]){
+                j = m - 1;
+            }
+            else 
+                return m;
         }
-        int n = nums.size();
-        if(nums.size() > 1 && nums[0] > nums[1]) return 0;
-        if(nums.size() > 1 && nums[n-1] > nums[n-2]) return n-1;
-        if(nums.size() == 1) return 0;
         return 0;
     }
 };
