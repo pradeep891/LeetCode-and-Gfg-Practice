5c3fa6065a40fc58a4e6eb990b58a43193261cc8
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-11-17 16:45:02 +0530

    Time: 28 ms (87.11%), Space: 19.3 MB (58.80%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,9 +4,7 @@ public:
         int j = 0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i] != 0)
-                nums[j++] = nums[i];
+                swap(nums[i] , nums[j++]);
         }
-        while(j < nums.size())
-            nums[j++] = 0;
     }
 };
