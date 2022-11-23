c4980d62ff7680f778209fdad58467fabe06a7b8
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-11-23 13:06:54 +0530

    Time: 286 ms (78.76%), Space: 34.6 MB (99.90%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -3,14 +3,26 @@ public:
     bool checkValid(vector<vector<int>>& matrix) {
         int n = matrix.size();
         for(int i=0; i<n; i++){
-            set<int>row , col;
+            
             for(int j=0; j<n; j++){
-                char a = matrix[i][j] , b = matrix[j][i];
-                // cout << a << " " << b << " | ";
-                if(row.find(a) != row.end()) return false;
-                if(col.find(b) != col.end()) return false;
-                row.insert(a);
-                col.insert(b);
+                int n = matrix[i][j];
+                int id = abs(n) - 1;
+                if(matrix[i][id] < 0) return false;
+                matrix[i][id] *= -1;
+            }
+            
+            
+            for(int j=0; j<n; j++){
+                matrix[i][j] = abs(matrix[i][j]);
+            }
+        }
+                
+        for(int i=0; i<n; i++){            
+            for(int j=0; j<n; j++){
+                int n = matrix[j][i];
+                int id = abs(n) - 1;
+                if(matrix[id][i] < 0) return false;
+                matrix[id][i] *= -1;
             }
         }
         return true;
