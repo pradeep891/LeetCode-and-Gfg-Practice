d12606778bd29a3a6d4563d7373a94008d821489
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2022-11-24 12:52:24 +0530

    Time: 1898 ms (5.01%), Space: 7.9 MB (94.48%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool dfs(int x , int y , vector<vector<int>>&vis , vector<vector<char>>&board , string word , int id){
+    bool dfs(int x , int y , vector<vector<char>>&board , string word , int id){
         // if(board[i][])
         
         int n  = board.size();
@@ -9,28 +9,28 @@ public:
         
         if(id == word.size()) return true;
         if(x >= n || x < 0 || y < 0 || y >= m )return false;
-        if(vis[x][y] == 1) return false;
+        if(board[x][y] == '.') return false;
         if(board[x][y] != word[id]) return false;
         
-        vis[x][y] = 1;
+        char ch = board[x][y];
+        board[x][y] = '.';
         
-        if(dfs(x+1 , y , vis , board , word , id+1)) return true;
-        if(dfs(x-1 , y , vis , board , word , id+1)) return true;
-        if(dfs(x , y+1 , vis , board , word , id+1)) return true;
-        if(dfs(x , y-1 , vis , board , word , id+1)) return true;
+        if(dfs(x+1 , y , board , word , id+1)) return true;
+        if(dfs(x-1 , y , board , word , id+1)) return true;
+        if(dfs(x , y+1 , board , word , id+1)) return true;
+        if(dfs(x , y-1 , board , word , id+1)) return true;
         
-        vis[x][y] = 0;
+        board[x][y] = ch;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n  = board.size();
         int m = board[0].size();
         
-        vector<vector<int>>vis(n , vector<int>(m , 0));
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(board[i][j] == word[0]){
-                    if(dfs(i , j , vis , board , word , 0)) return true;
+                    if(dfs(i , j , board , word , 0)) return true;
                 }
             }
         }
