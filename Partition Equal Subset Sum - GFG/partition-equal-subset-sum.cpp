d7db015a1f19ebf7ae38126fd2869ed922acc95b
Pradeep Kumar Sahu <63865783+pradeep891@users.noreply.github.com>
2023-09-15 02:12:56 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -10,16 +10,16 @@ using namespace std;
 class Solution{
 public:
 
-    bool solve(int ar[], int n , int sum, vector<vector<int>>&dp){
-        if(sum == 0) return true;
-        if(sum < 0) return false;
-        if(n == 0) return false;
+    // bool solve(int ar[], int n , int sum, vector<vector<int>>&dp){
+    //     if(sum == 0) return true;
+    //     if(sum < 0) return false;
+    //     if(n == 0) return false;
         
-        if(dp[n][sum] != -1) return dp[n][sum];
-        bool a = solve(ar ,  n-1, sum, dp);
-        bool b = solve(ar,  n-1 , sum - ar[n-1], dp);
-        return dp[n][sum] = a || b;
-    }
+    //     if(dp[n][sum] != -1) return dp[n][sum];
+    //     bool a = solve(ar ,  n-1, sum, dp);
+    //     bool b = solve(ar,  n-1 , sum - ar[n-1], dp);
+    //     return dp[n][sum] = a || b;
+    // }
     int equalPartition(int N, int arr[])
     {
         // code here
@@ -28,8 +28,20 @@ public:
         if(sum % 2 == 1) return 0;
         sum /= 2;
         
-        vector<vector<int>>dp(N+1, vector<int>(sum+1, -1));
-        return solve(arr , N , sum , dp);
+        vector<vector<bool>>dp(N+1, vector<bool>(sum+1, false));
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<=sum; j++){
+                if(j == 0) {
+                    dp[i][j] = true;
+                    continue;
+                }
+                dp[i][j] = dp[i-1][j];
+                if(j-arr[i-1] >= 0) 
+                dp[i][j] =  dp[i-1][j] || dp[i-1][j-arr[i-1]];
+            }
+        }
+        return dp[N][sum];
+        // return solve(arr , N , sum , dp);
     }
 };
 
